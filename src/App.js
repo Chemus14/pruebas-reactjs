0402459d68ce5fb5cb2e1b386a649c5be5e4e41d
Chemus14 <jmjimgar@gmail.com>
2022-04-02 12:24:04 +0100

    aa

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to playaaasssa.
+          Edit <code>src/App.js</code> and save to playaaassssa.
         </p>
         <a
           className="App-link"
