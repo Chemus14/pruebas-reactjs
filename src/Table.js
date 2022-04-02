f3a9b7af248d3062011da9a1ff8943ea955b1dc7
Chemus14 <jmjimgar@gmail.com>
2022-04-02 20:28:38 +0100

    update

--- src/Table.js
+++ src/Table.js
@@ -1,6 +1,4 @@
 import React, {Component} from 'react';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Table from 'react-bootstrap/Table';
 class Tables extends Component {
     render() {
